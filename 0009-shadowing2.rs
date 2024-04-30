0e822c40af3709e7ba725345107d6ffca5dcc2b6
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-04-30 14:18:22 +0300

    Create 0009-shadowing2.rs

---
+++ 0009-shadowing2.rs
@@ -0,0 +1,16 @@
+fn main() {
+
+    let year = 2024;
+
+    println!("{year}"); // 2024
+
+    let year = 1993;
+
+    println!("{year}"); // 1993
+
+    let year = 2018;
+
+    println!("{year}"); // 2018
+
+}
+
