eae9c13eece06d1d963601a5594e28493238b78b
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-04-30 13:48:29 +0300

    Create 0004-format.rs

---
+++ 0004-format.rs
@@ -0,0 +1,12 @@
+fn main() {
+
+    let name = "Mustafa Buyukdereli";
+
+    let year: u16 = 2024;
+
+    let sentence = format!("{name} is here in {year}.");
+
+    println!("{sentence}"); //Mustafa Buyukdereli is here in 2024.
+    
+}
+
