3a3bde30389a5b261facc188f27abccce83b01dc
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-04-30 14:08:04 +0300

    Create 0007-single-quotes.rs

---
+++ 0007-single-quotes.rs
@@ -0,0 +1,18 @@
+fn main() {
+
+    let a: char = 'a'; // single quotes
+
+    let b = '1';
+
+    let c = '😭';
+
+    //let d = 'Mustafa'; // Error! Use double quotes!
+
+    println!("{a}"); // a
+
+    println!("{b}"); // 1
+
+    println!("{c}"); // 😭
+
+}
+
