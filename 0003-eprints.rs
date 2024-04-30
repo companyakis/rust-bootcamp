ae87190f55aafd401db7f94a950c5625029abf0b
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-04-30 13:45:29 +0300

    Create 0003-eprints.rs

---
+++ 0003-eprints.rs
@@ -0,0 +1,11 @@
+fn main() {
+
+    let keyword = "Rust Bootcamp 2024!";
+
+    eprint!("{keyword} "); // Rust Bootcamp 2024!
+
+    eprintln!("{}", keyword); // Rust Bootcamp 2024!
+    
+}
+
+// Rust Bootcamp 2024! Rust Bootcamp 2024!
