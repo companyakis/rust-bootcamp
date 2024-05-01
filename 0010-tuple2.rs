e51a3026843fda9aa530653d4bad2dd953ccdffd
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-05-01 16:45:03 +0300

    Create 0010-tuple2.rs

---
+++ 0010-tuple2.rs
@@ -0,0 +1,9 @@
+fn main() {
+
+    let founder_info = ("Mustafa", "Büyükdereli", 1, true); //immutable
+
+    let name = founder_info.0; //tuple.0, tuple.1 etc
+
+    println!("Name: {name}"); //Name: Mustafa
+
+}
