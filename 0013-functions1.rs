898258628e27791c34eb176e4b30f003cf3a59e5
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-05-01 22:10:00 +0300

    Create 0013-functions1.rs

---
+++ 0013-functions1.rs
@@ -0,0 +1,23 @@
+fn main() {
+
+   greeting("Mustafa");
+    
+   void_hi();
+
+}
+
+fn greeting(name: &str) {
+
+    println!("Hi {name}!");
+
+}
+
+fn void_hi() {
+    println!("Hi there!");
+}
+
+/*
+Hi Mustafa!
+Hi there!
+*/
+
