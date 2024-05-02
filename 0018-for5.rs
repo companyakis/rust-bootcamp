bcfeb48f3529767db72fa25fa6b7529eb67a2289
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-05-02 10:16:36 +0300

    Create 0018-for5.rs

---
+++ 0018-for5.rs
@@ -0,0 +1,23 @@
+fn main() {
+
+    for (i, counter) in (1..=10).enumerate() {
+
+        println!("Counter_{i}: {counter} ");
+    }
+
+}
+
+/*
+Counter_0: 1 
+Counter_1: 2  
+Counter_2: 3  
+Counter_3: 4  
+Counter_4: 5  
+Counter_5: 6  
+Counter_6: 7  
+Counter_7: 8  
+Counter_8: 9  
+Counter_9: 10 
+*/
+
+
