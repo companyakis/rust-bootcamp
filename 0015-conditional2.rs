b751bf3282bd4082287e5ea5f735a2a7e962906e
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-05-02 09:36:29 +0300

    Create 0015-conditional2.rs

---
+++ 0015-conditional2.rs
@@ -0,0 +1,7 @@
+fn main() {
+
+    let number: i16 = -102;
+
+    if number > 100 { println!("Number > 100") } else if number < 100 { println!("Number <  100") } else { println!("Number is equal to 100")};
+
+}
