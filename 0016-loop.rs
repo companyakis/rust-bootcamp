239815f074a054fadd9aaf6c12f848cda50c9658
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-05-02 09:40:50 +0300

    Create 0016-loop.rs

---
+++ 0016-loop.rs
@@ -0,0 +1,22 @@
+fn main() {
+
+    let mut counter: u8 = 0;
+
+    loop {
+
+        counter += 1;
+
+        print!("{counter} ");
+
+        if counter == 34 {
+
+            break;
+        }
+    }
+}
+
+
+/*
+1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31 32 33 34 
+*/
+
