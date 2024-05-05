6ac2090d3e656ca186bd7f7948ce55db0ca7d941
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-05-05 22:37:39 +0300

    Create 0019-match2.rs

---
+++ 0019-match2.rs
@@ -0,0 +1,21 @@
+fn main() {
+
+    let year: u16 = 2024;
+
+    match year {
+
+        2018 => println!("Re-birth"),
+
+        2019 | 2020 | 2021 | 2022 | 2023 => println!("Waiting..."),
+
+        2024 => println!("Wake up Neo!"),
+
+        _ => println!("We don't have any information..."),
+    }
+
+}
+
+/*
+Wake up Neo!
+*/
+
