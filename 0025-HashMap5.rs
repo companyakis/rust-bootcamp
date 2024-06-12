7f3f083330c9218c0fce5cd41fa19b49a633bee3
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-06-12 22:38:04 +0300

    Create 0025-HashMap5.rs

---
+++ 0025-HashMap5.rs
@@ -0,0 +1,33 @@
+use std::collections::HashMap;
+
+fn main() {
+
+    let mut salaries: HashMap<String, u32> = HashMap::new();
+
+    salaries.insert(String::from("Mustafa"), 80000);
+
+    salaries.insert(String::from("Aygun"), 50000);
+
+    salaries.insert(String::from("Kagan"), 45000);
+
+    // Iterating  
+
+    // for (k, v) in salaries { //we should borrow
+
+    //     println!("Name: {k} and salary: {v}");
+    // }
+
+    //println!("Salaries: {:?}", &salaries); // borrow of moved value: `salaries` value borrowed here after move
+
+    for (k, v) in &salaries { //we should borrow
+
+        println!("Name: {k} and salary: {v}");
+    }
+    
+}
+
+/*
+Name: Mustafa and salary: 80000
+Name: Aygun and salary: 50000
+Name: Kagan and salary: 45000
+*/
