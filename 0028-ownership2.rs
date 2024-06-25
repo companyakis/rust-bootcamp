06431168f5897287cfecec8572f6bc256e94687a
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-06-25 23:43:23 +0300

    Create 0028-ownership2.rs

---
+++ 0028-ownership2.rs
@@ -0,0 +1,13 @@
+fn main() {
+
+    let string_1 = String::from("Mustafa");
+
+    println!("{string_1}"); // Mustafa
+
+    let string_2 = string_1.clone();
+
+    println!("{string_2}"); // Mustafa
+
+    println!("{string_1}"); // Mustafa
+}
+
