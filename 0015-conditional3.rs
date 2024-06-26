210bb20f223f8e7b315fa957ec98b3383683f717
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-06-26 22:23:59 +0300

    Create 0015-conditional3.rs

---
+++ 0015-conditional3.rs
@@ -0,0 +1,12 @@
+fn main() {
+
+    let x = 33;
+
+    let y = 46;
+
+    let selection = if x > y {x} else {y};
+
+    println!("Selection is equal to {selection}."); // Selection is equal to 46.
+
+}
+
