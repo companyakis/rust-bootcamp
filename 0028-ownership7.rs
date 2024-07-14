f903db114bf11673fc20e96ebcfd4feb92c36278
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-07-14 20:08:31 +0300

    Create 0028-ownership7.rs

---
+++ 0028-ownership7.rs
@@ -0,0 +1,15 @@
+fn main() {
+
+    let year: u16 = 2024;
+
+    let this_year = year;
+
+    // Can we use year variable? Yes! Why?.. The key word is "STACK"
+
+    println!("Year: {year}"); // Year: 2024
+
+    println!("This Year: {this_year}"); // This Year: 2024
+
+}
+
+
