91d8cc6fab0a41b13b6b950452541f1211e22962
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-07-14 19:44:37 +0300

    Create 0030-closures1.rs

---
+++ 0030-closures1.rs
@@ -0,0 +1,17 @@
+fn main() {
+
+    let sum_numbers = |a, b, c| a + b + c;
+
+    let a = 22;
+
+    let b= -11;
+
+    let c= 2221;
+    
+    let result = sum_numbers(a, b, c);
+
+    println!("{a} + {b} + {c} = {result}"); // 22 + -11 + 2221 = 2232
+
+}
+
+
