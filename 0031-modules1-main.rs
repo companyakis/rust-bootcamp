492ee5f542e8742e64bdd18bf7cb2de3e8f4860b
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-07-15 11:05:18 +0300

    Create 0031-modules1-main.rs

---
+++ 0031-modules1-main.rs
@@ -0,0 +1,13 @@
+mod user_methods;
+
+fn main() {
+
+    user_methods::greeting("Mustafa"); // Hello Mustafa!
+
+    let perimeter = user_methods::calculate_perimeter(15.0, 22.5);
+
+    println!("Perimeter: {perimeter}"); // Perimeter: 337.5
+
+}
+
+
