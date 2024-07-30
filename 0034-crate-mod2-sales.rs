4f5dcfc90806e61f7116e190cbf3d35b62e9314c
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-07-30 14:55:41 +0300

    Create 0034-crate-mod2-sales.rs

---
+++ 0034-crate-mod2-sales.rs
@@ -0,0 +1,15 @@
+pub mod sales_department {
+
+    pub fn sales_department_warning() {
+
+        println!("Please, check the inventory!");
+    }
+
+    // private
+
+    fn sales_department_hidden_warning_message() {
+
+        println!("Sales are decreasing!");
+    }
+}
+    
