8fd6681c9ec731644eca1b04ab0bae6004b2b2de
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-07-31 09:58:02 +0300

    Create 0902-cli-project2-step1.rs

---
+++ 0902-cli-project2-step1.rs
@@ -0,0 +1,15 @@
+fn main() {
+
+    println!("___________EMPLOYEE BONUS SYSTEM______________________");
+
+    println!("___________Please, Select:____________________________");
+
+    println!("___________1- Add a New Employee______________________");
+
+    println!("___________2- Show Employee Information_______________");
+
+    println!("___________3- Delete Employee Information_____________");
+
+    println!("___________4- Update Employee Information_____________");
+
+}
