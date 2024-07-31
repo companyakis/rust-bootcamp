74536edd839ecc910517831d39939928bdf20b88
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-07-31 10:19:41 +0300

    Create 0902-cli-project4.rs

---
+++ 0902-cli-project4.rs
@@ -0,0 +1,14 @@
+// Employee info to calculate bonus
+
+struct Employee {
+  
+    name: String,
+  
+    id: u16,
+  
+    first_two_weekly_sales: f32,
+  
+    second_two_weekly_sales: f32,
+  
+    result: String
+}
