5bd71bde50eee2acbea988f0f372ec328e64a394
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-09-16 11:20:41 +0300

    Create 0043-lifetimes4-2.rs

---
+++ 0043-lifetimes4-2.rs
@@ -0,0 +1,32 @@
+fn main() {
+
+    // lifetime
+
+    let e1 = Employee {name: "Hakan".to_string(), id: 142, salary: 24_000};
+
+    let e2 = Employee {name: "Aybilge".to_string(), id: 254, salary: 21_000};
+
+    let result = find_higher_salary_owner(&e1, &e2); 
+
+    println!("Higher salary owner: {:?}", result); // Higher salary owner: Employee { name: "Hakan", id: 142, salary: 24000 }
+
+}
+
+#[derive(Debug)]
+struct Employee {
+    name: String,
+    id: u16,
+    salary: u16
+}
+
+fn find_higher_salary_owner<'a>(e1: & 'a Employee, e2: & 'a Employee) -> & 'a Employee {
+
+    if e1.salary > e2.salary {
+        e1
+    }
+
+    else {
+
+        e2
+    }
+}
