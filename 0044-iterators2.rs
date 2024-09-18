9dda01608e29d686e017ecb0e1ddabecc6e82887
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-09-18 12:01:08 +0300

    Create 0044-iterators2.rs

---
+++ 0044-iterators2.rs
@@ -0,0 +1,28 @@
+fn main() {
+
+    let years: [u16; 6] = [2018, 2019, 2020, 2021, 2022, 2023];
+
+    let mut year_iterator = years.iter();
+
+    // next method
+
+    println!("Year: {:?}", year_iterator.next());
+    println!("Year: {:?}", year_iterator.next());
+    println!("Year: {:?}", year_iterator.next());
+    println!("Year: {:?}", year_iterator.next());
+    println!("Year: {:?}", year_iterator.next());
+    println!("Year: {:?}", year_iterator.next());
+    println!("Year: {:?}", year_iterator.next());
+    println!("Year: {:?}", year_iterator.next());
+
+// Year: Some(2018)
+// Year: Some(2019)
+// Year: Some(2020)
+// Year: Some(2021)
+// Year: Some(2022)
+// Year: Some(2023)
+// Year: None      
+// Year: None    
+}
+
+
