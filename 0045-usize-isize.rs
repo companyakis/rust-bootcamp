fc098c81e6e47a8b3ea2a9bd2287d8a3302bac4e
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-09-30 22:13:46 +0300

    Create 0045-usize-isize.rs

---
+++ 0045-usize-isize.rs
@@ -0,0 +1,18 @@
+fn main() {
+
+    let x: usize = 2024;
+
+    let y: usize = 2_192_887_965_987;
+
+    println!("x and y: {x} - {y}");
+
+    let z: isize = 6500;
+
+    let t: isize = -345_887_981;
+
+    println!("z and t: {z} - {t}")
+    
+}
+
+// x and y: 2024 - 2192887965987
+// z and t: 6500 - -345887981
