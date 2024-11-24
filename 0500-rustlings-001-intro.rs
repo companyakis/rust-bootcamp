a35147b8b46a4691fa4370f75934ef1a1fc749f1
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-11-24 23:11:34 +0300

    Create 0500-rustlings-001-intro.rs

---
+++ 0500-rustlings-001-intro.rs
@@ -0,0 +1,9 @@
+// fn main() {
+//     // TODO: Fix the code to print "Hello world!".
+//     printline!("Hello world!");
+// }
+
+fn main() {
+    // TODO: Fix the code to print "Hello world!".
+    println!("Hello world!");
+}
