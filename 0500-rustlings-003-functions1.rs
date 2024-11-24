08a02a86c51276d7b237b20f62ed4a5eeb25f433
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-11-24 23:22:07 +0300

    Create 0500-rustlings-003-functions1.rs

---
+++ 0500-rustlings-003-functions1.rs
@@ -0,0 +1,14 @@
+// // TODO: Add some function with the name `call_me` without arguments or a return value.
+
+// fn main() {
+//     call_me(); // Don't change this line
+// }
+
+// TODO: Add some function with the name `call_me` without arguments or a return value.
+fn call_me() {
+    
+}
+
+fn main() {
+    call_me(); // Don't change this line
+}
