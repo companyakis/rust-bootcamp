21cf31de5d7fc360ba6b99bb135d127fe7cdfcb1
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-12-03 15:47:02 +0300

    Create 0400-0001-005-Constants.rs

---
+++ 0400-0001-005-Constants.rs
@@ -0,0 +1,15 @@
+const PI_VALUE: f64 = 22.0 / 7.0;
+
+const FOUNDER: &str = "Mustafa Büyükdereli";
+
+
+fn main() {
+
+    println!("{PI_VALUE}");
+    
+     println!("{FOUNDER}")
+
+}
+
+// 3.142857142857143
+// Mustafa Büyükdereli
