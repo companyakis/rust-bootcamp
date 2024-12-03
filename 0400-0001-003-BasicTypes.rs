3ac96355624b88a056a504a93f3e04dabef4284d
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-12-03 15:39:25 +0300

    Create 0400-0001-003-BasicTypes.rs

---
+++ 0400-0001-003-BasicTypes.rs
@@ -0,0 +1,23 @@
+fn main() {
+
+    // some basic examples
+
+    let ready: bool = false;
+    
+    let age = 87u8;
+    
+    let diff: i16 = -5000;
+    
+    let portion: f32 = 12.75;
+    
+    let total: u128 = 6_454_214_468;
+    
+    let first_char: char = 'a';
+    
+    let info: (&str, &str, u8) = ("Mustafa", "FinTech", 101);
+    
+    let years = [2018, 2020, 2024];
+    
+    let my_name = "Mustafa";
+
+}
