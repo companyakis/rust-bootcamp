f33fef3f4095ea0639af43fd395259b20d595ee4
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-12-04 21:35:38 +0300

    Create 0400-0002-001-Conditionals.rs

---
+++ 0400-0002-001-Conditionals.rs
@@ -0,0 +1,21 @@
+fn main() {
+
+    let year: u16 = 1990;
+    
+    if year < 2024 {
+        
+        println!("Past...")
+    } 
+    
+    else if year == 2024 {
+        
+        println!("Current year!")
+    }
+    
+    else {
+        
+        println!("Future...")
+    }
+
+
+}
