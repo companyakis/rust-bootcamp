537810b1287c77ee883452806cd63bd40eccc1ed
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-12-04 19:20:05 +0300

    Create 0400-0001-006-Arrays.rs

---
+++ 0400-0001-006-Arrays.rs
@@ -0,0 +1,11 @@
+fn main() {
+
+    let ages: [u8; 5] = [22, 34, 19, 98, 47];
+    
+    println!("{:?}", ages);
+    
+    let names: [&str; 3] = ["Aygün", "Kağan", "Bilge"];
+    
+    println!("{:?}", names[2]);
+    
+}
