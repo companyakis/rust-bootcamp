0595805edb298453793d90097213218ebf93d41a
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-12-05 19:29:53 +0300

    Create 0400-0002-003-While.rs

---
+++ 0400-0002-003-While.rs
@@ -0,0 +1,17 @@
+    let mut counter: i8 = -11;
+    
+    while counter < 50 {
+    
+        println!("Counter: {counter}");
+        
+        counter = -2 * counter + 1;
+    }
+    
+    println!("Counter final: {counter}")
+
+}
+
+// Counter: -11
+// Counter: 23
+// Counter: -45
+// Counter final: 91
